f3e77b442bbf63afcd8ee49ca140c1f2dd5dac8b
OrencioHarveyAlexis <harveyorencio@gmail.com>
2022-04-14 21:23:56 +0800

    second commit

---
+++ song2.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tune Avenue </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tune Avenue</h1>
+                <p class="lead">Melancholy runs through music like a message through a stick of rock </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Natural</h2>";
+                        echo "<p class='lead '>Imagine Dragons</p>";
+                
+                        echo "<p> Will you hold the line </p>";
+                        echo "<p> When every one of them has given up and given in, tell me</p>";
+                        echo "<p> In this house of mine</p>";
+                        echo "<p> Nothing ever comes without a consequence or cost, tell me</p>";
+                        echo "<p> Will the stars align?</p>";
+                        echo "<p> Will heaven step in? Will it save us from our sin? Will it</p>";
+                        echo "<p> 'Cause this house of mine stands strong</p>";
+                        echo "<br/>";
+
+                        echo "<p> That's the price you pay</p>";
+                        echo "<p> Leave behind your heart and cast away</p>";
+                        echo "<p> Just another product of today</p>";
+                        echo "<p> Rather be the hunter than the prey</p>";
+                        echo "<p> And you're standing on the edge, face up 'cause you're a...</p>";
+                        echo "<br/>";
+
+                        echo "<p> Natural</p>";
+                        echo "<p> A beating heart of stone</p>";
+                        echo "<p> You gotta be so cold</p>";
+                        echo "<p> To make it in this world</p>";
+                        echo "<p> Yeah, you're a natural</p>";
+                        echo "<p> Living your life cutthroat</p>";
+                        echo "<p> You gotta be so cold</p>";
+                        echo "<p> Yeah, you're a natural</p>";                    
+                        echo "<br/>";
+
+                        echo "<p> Will somebody</p>";
+                        echo "<p> Let me see the light within the dark trees shadowing</p>";
+                        echo "<p> What's happening?</p>";
+                        echo "<p> Looking through the glass find the wrong within the past knowing</p>";
+                        echo "<p> Oh, we are the youth</p>";
+                        echo "<p> Cut until it bleeds inside a world, without the peace, face it</p>";
+                        echo "<p> A bit of the truth, the truth</p>";
+                        echo "<br/>";
+
+                        echo "<p> That's the price you pay</p>";
+                        echo "<p> Leave behind your heart and cast away</p>";
+                        echo "<p> Just another product of today</p>";
+                        echo "<p> Rather be the hunter than the prey</p>";
+                        echo "<p> And you're standing on the edge, face up 'cause you're a...</p>";
+                        echo "<br/>";
+
+                        echo "<p> Natural</p>";
+                        echo "<p> A beating heart of stone</p>";
+                        echo "<p> You gotta be so cold</p>";
+                        echo "<p> To make it in this world</p>";
+                        echo "<p> Yeah, you're a natural</p>";
+                        echo "<p> Living your life cutthroat</p>";
+                        echo "<p> You gotta be so cold</p>";
+                        echo "<p> Yeah, you're a natural</p>";
+                        echo "<br/>";
+
+                        echo "<p> Deep inside me, I'm fading to black, I'm fading</p>";
+                        echo "<p> Took an oath by the blood of my hand, won't break it</p>";
+                        echo "<p> I can taste it, the end is upon us, I swear</p>";
+                        echo "<p> Gonna make it</p>";
+                        echo "<p> I'm gonna make it</p>";
+                        echo "<br/>";
+
+                         echo "<p> Natural</p>";
+                        echo "<p> A beating heart of stone</p>";
+                        echo "<p> You gotta be so cold</p>";
+                        echo "<p> To make it in this world</p>";
+                        echo "<p> Yeah, you're a natural</p>";
+                        echo "<p> Living your life cutthroat</p>";
+                        echo "<p> You gotta be so cold</p>";
+                        echo "<p> Yeah, you're a natural</p>";
+                        echo "<br/>";
+
+                        echo "<p> Natural</p>";
+                        echo "<p> Yeah, you're a natural</p>";
+
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ryan Clifford Perez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
